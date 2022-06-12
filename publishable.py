fce2dba05b24c857bb6901acc0f24893ca7e262a
MarkPellowe <77686488+MarkPellowe@users.noreply.github.com>
2022-06-12 23:22:22 +0100

    Update publishable.py

--- publishable.py
+++ publishable.py
@@ -1,109 +1,92 @@
 """A template to allow plotly to be used instead of matplotlib DIRECTLY in 
     the thesis. 
-
     This file needs to be within the path, and you import it at the beginning of 
     the document and then set the default template to the one desired as:
-
     `import publishable
     pio.templates.default = "scientific"`
-
     """
 
 import plotly.graph_objects as go
 import plotly.io as pio
 
-pio.templates["draft"] = go.layout.Template(
-    layout_annotations=[
-        dict(
-            name="draft watermark",
-            text="DRAFT",
-            textangle=-30,
-            opacity=0.1,
-            font=dict(color="black", size=100),
-            xref="paper",
-            yref="paper",
-            x=0.5,
-            y=0.5,
-            showarrow=False,
-        )
-    ]
-)
 
 pio.templates["scientific"] = go.layout.Template(
     layout=dict(
         scene={
             "xaxis": {
                 "backgroundcolor": "white",
-                "gridcolor": "rgb(232,232,232)",
+                "gridcolor": "rgb(45,45,45)",
                 "gridwidth": 2,
-                "linecolor": "rgb(36,36,36)",
+                "linecolor": "rgb(0,0,0)",
                 "showbackground": True,
                 "showgrid": True,
                 "showline": True,
-                "mirror": True,
+                "mirror": "allticks",
                 "ticks": "inside",
                 "nticks": 6,
-                "tickwidth": 1.25,
+                "tickwidth": 1.5,
                 "zeroline": False,
-                "zerolinecolor": "rgb(36,36,36)",
+                "zerolinecolor": "rgb(0,0,0)",
             },
             "yaxis": {
                 "backgroundcolor": "white",
-                "gridcolor": "rgb(232,232,232)",
+                "gridcolor": "rgb(45,45,45)",
                 "gridwidth": 2,
-                "linecolor": "rgb(36,36,36)",
+                "linecolor": "rgb(0,0,0)",
                 "showbackground": True,
                 "showgrid": True,
                 "showline": True,
-                "mirror": True,
+                "mirror": "allticks",
                 "ticks": "inside",
                 "nticks": 6,
-                "tickwidth": 1.25,
+                "tickwidth": 1.35,
                 "zeroline": False,
-                "zerolinecolor": "rgb(36,36,36)",
+                "zerolinecolor": "rgb(0,0,0)",
             },
             "zaxis": {
                 "backgroundcolor": "white",
-                "gridcolor": "rgb(232,232,232)",
+                "gridcolor": "rgb(45,45,45)",
                 "gridwidth": 2,
-                "linecolor": "rgb(36,36,36)",
+                "linecolor": "rgb(0,0,0)",
                 "showbackground": True,
                 "showgrid": True,
                 "showline": True,
-                "mirror": True,
+                "mirror": "allticks",
                 "ticks": "inside",
                 "nticks": 6,
-                "tickwidth": 1.25,
+                "tickwidth": 1.5,
                 "zeroline": False,
-                "zerolinecolor": "rgb(36,36,36)",
+                "zerolinecolor": "rgb(0,0,0)",
             },
         },
         xaxis={
             "automargin": True,
-            "gridcolor": "rgb(232,232,232)",
-            "linecolor": "rgb(36,36,36)",
+            "gridcolor": "rgb(45,45,45)",
+            "linecolor": "rgb(0,0,0)",
             "linewidth": 2,
             "showgrid": True,
             "showline": True,
-            "mirror": True,
+            "mirror": "allticks",
             "ticks": "inside",
             "nticks": 6,
-            "tickwidth": 1.25,
+            "tickwidth": 1.5,
             "title": {"standoff": 10},
             "zeroline": False,
+            "griddash": "dash",
         },
         yaxis={
             "automargin": True,
-            "gridcolor": "rgb(232,232,232)",
-            "linecolor": "rgb(36,36,36)",
+            "gridcolor": "rgb(45,45,45)",
+            "linecolor": "rgb(0,0,0)",
             "linewidth": 2,
             "showgrid": True,
             "showline": True,
-            "mirror": True,
+            "mirror": "allticks",
             "ticks": "inside",
             "nticks": 6,
-            "tickwidth": 1.25,
+            "tickwidth": 1.5,
             "title": {"standoff": 10},
+            "griddash": "dash",
             "zeroline": False,
         },
         font={"family": "serif", "size": 12, "color": "black"},
